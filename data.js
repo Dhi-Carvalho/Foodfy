b04fc70c3760c8f03e6e821613d6b46af142a0a3
Diogo Santana <dhi.carvalho85@gmail.com>
2020-07-31 09:19:09 -0300

    add data file

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "Burger",
+        image: "/assets/burger.png",
+        name: "Triplo bacon burger",
+        author: "Jorge Relato",
+    },
+    {
+        id: "Pizza",
+        image: "/assets/pizza.png",
+        name: "Pizza 4 estações",
+        author: "Fabiana Melo",
+    },
+    {
+        id: "Espaguete",
+        image: "/assets/espaguete.png",
+        name: "Espaguete ao alho",
+        author: "Júlia Kinoto",
+    },
+    {
+        id: "Lasanha",
+        image: "/assets/lasanha.png",
+        name: "Lasanha mac n' cheese",
+        author: "Juliano Vieira",
+    },
+    {
+        id: "Doce",
+        image: "/assets/doce.png",
+        name: "Docinhos pão-do-céu",
+        author: "Ricardo Golvea",
+    },
+    {
+        id: "Asinhas",
+        image: "/assets/asinhas.png",
+        name: "Asinhas de frango ao barbecue",
+        author: "Vania Steroski",
+    }
+]
