7a56b78a9d0ba79cd6907894ed0abadf1b33e7c2
Diogo Santana <dhi.carvalho85@gmail.com>
2020-08-27 16:24:15 -0300

    use routes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const routes = require("./routes")
 
 const server = express()
-const recipes = require("./src/database/data")
 
 server.use(express.static("public"))
+server.use(routes)
 
 server.set("view engine", "njk")
 
@@ -12,31 +13,6 @@ nunjucks.configure("src/views", {
     express: server,
     noCache: true
 })
-
-server.get("/", function(req, res) {
-    return res.render("index", {items: recipes})
-})
-
-server.get("/about", function(req, res) {
-    return res.render("about")
-})
-
-server.get("/cookbook", function(req, res) {
-    return res.render("cookbook", {items: recipes})
-})
-
-server.get("/recipes/:index", function(req,res) {
-    const recipeId = req.params.index
-
-    if (!recipes[recipeId]) {
-        return res.render('not-found')
-    }
-    return res.render('recipes', { item: recipes[recipeId]})
-})
-
-server.use(function(req,res){
-    return res.status(404).render('not-found')
-  })
 
 server.listen(5000, function(){
     console.log("server is running")
